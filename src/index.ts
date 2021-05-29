956bd74e31f824fad41bb0fd1c44725b9dd768b1
Adonys Santos <adonysmundotech@gmail.com>
2021-05-29 02:41:46 -0400

    feat: Migrate js to typescript

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,27 @@
 // Environment Variables
+import { config } from "dotenv";
 if (process.env.NODE_ENV !== "production") {
   const NODE_ENV = process.env.NODE_ENV;
-  require("dotenv").config({ path: `.env.${NODE_ENV}` });
+  config({ path: `.env.${NODE_ENV}` });
 }
 
 const BOT_PREFIX = process.env.PREFIX;
 const BOT_TOKEN = process.env.TOKEN;
 
 // Discord Bot
-const Discord = require("discord.js");
-const client = new Discord.Client();
+import { Client } from "discord.js";
+const client: Client = new Client();
 
 client.on("ready", () => {
   // Custom presence
-  client.user.setPresence({
+  client.user?.setPresence({
     activity: {
       name: "I can see you!",
       type: "WATCHING",
     },
     status: "online",
   });
-  console.log(`Logged in as: ${client.user.tag}!`);
+  console.log(`Logged in as: ${client.user?.tag}!`);
 });
 
 client.login(BOT_TOKEN);
