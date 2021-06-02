70abdfd5ae681b71ad09d9887ba32d99a4f76aa0
Adonys Santos <adonysmundotech@gmail.com>
2021-06-01 22:39:37 -0400

    feat: Add firebase config

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,5 +1,26 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
 import getEnvVar from "./helpers/getEnvVar";
 
-const firebase = {};
+const apiKey = getEnvVar("FIREBASE_API_KEY");
+const authDomain = getEnvVar("FIREBASE_AUTH_DOMAIN");
+const projectId = getEnvVar("FIREBASE_PROJECT_ID");
+const storageBucket = getEnvVar("FIREBASE_STORAGE_BUCKET");
+const messagingSenderId = getEnvVar("FIREBASE_MESSAGING_SENDER_ID");
+const appId = getEnvVar("FIREBASE_APP_ID");
+const measurementId = getEnvVar("FIREBASE_MEASUREMENT_ID");
 
-export default firebase;
+const firebaseConfig = {
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId,
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+export { db, firebase };
