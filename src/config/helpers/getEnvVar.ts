59d2706e88fe6a4a540a1b4c83217ff49b53bdc3
Adonys Santos <adonysmundotech@gmail.com>
2021-06-01 21:10:12 -0400

    refactor: Move discord settings to config folder

---
+++ src/config/helpers/getEnvVar.ts
@@ -0,0 +1,15 @@
+// Environment Variables
+import { config } from "dotenv";
+if (process.env.NODE_ENV !== "production") {
+  const NODE_ENV = process.env.NODE_ENV;
+  config({ path: `.env.${NODE_ENV}` });
+}
+
+const getEnvVar = (value: string): string => {
+  const ret = process.env[value];
+  if (ret === undefined)
+    throw new Error(`${process.env}.${value} is undefined!`);
+  return ret;
+};
+
+export default getEnvVar;
