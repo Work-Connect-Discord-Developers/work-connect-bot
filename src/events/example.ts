7d70974209cda226ba16804fa1ef5b5d6d642d9b
Adonys Santos <adonysmundotech@gmail.com>
2021-07-03 11:27:31 -0400

    feat: Add event handler

---
+++ src/events/example.ts
@@ -0,0 +1,10 @@
+import { Client } from "discord.js";
+
+const event = {
+  name: "",
+  run: async (client: Client /*[others parameter]*/) => {
+    // Event code...
+  },
+};
+
+export default event;
---
+++ src/events/messages.ts
@@ -0,0 +1,29 @@
+import { Client, Message } from "discord.js";
+import { discord } from "../config/index";
+
+const event = {
+  name: "message",
+  run: async (client: Client, msg: Message) => {
+    if (msg.author.bot) return;
+    if (msg.channel.type === "dm") return;
+    if (!msg.content.startsWith(discord.BOT_PREFIX)) return;
+
+    // Arguments are all messages after prefix and command
+    const PREFIX_LENGTH: number = discord.BOT_PREFIX.length;
+    const ARGUMENTS: string[] = msg.content
+      .slice(PREFIX_LENGTH)
+      .trim()
+      .split(/ +/g);
+    const COMMAND: string = ARGUMENTS.shift()?.toLocaleLowerCase()!;
+
+    // Command Handler
+    try {
+      const COMMAND_FILE = require(`../commands/${COMMAND}`) || null;
+      if (COMMAND_FILE) COMMAND_FILE.default.run(msg, ARGUMENTS);
+    } catch (error) {
+      msg.channel.send("This command does not exist!!");
+    }
+  },
+};
+
+export default event;
