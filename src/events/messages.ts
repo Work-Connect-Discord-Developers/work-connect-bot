e51d055dd2b7b9708bb188811e718ca1a5d3072a
Adonys Santos <adonysmundotech@gmail.com>
2021-07-05 07:18:47 -0400

    perf: Add await to command handler

--- src/events/messages.ts
+++ src/events/messages.ts
@@ -1,13 +1,13 @@
-import { Message } from "discord.js";
-import { Event } from "../interfaces";
-import { discord } from "../config/index";
-import permissionsChecker from "../helpers/permissionsChecker";
+import { Message } from 'discord.js';
+import { Event } from '../interfaces';
+import { discord } from '../config/index';
+import permissionsChecker from '../helpers/permissionsChecker';
 
 const event: Event = {
-  name: "message",
+  name: 'message',
   run: async (client, msg: Message) => {
     if (msg.author.bot) return;
-    if (msg.channel.type === "dm") return;
+    if (msg.channel.type === 'dm') return;
     if (!msg.content.startsWith(discord.BOT_PREFIX)) return;
 
     // Arguments are all messages after prefix and command
@@ -20,7 +20,7 @@ const event: Event = {
 
     // Command Handler
     try {
-      const COMMAND_FILE = require(`../commands/${COMMAND}`);
+      const COMMAND_FILE = await require(`../commands/${COMMAND}`);
       const userHasPermissions = permissionsChecker(
         COMMAND_FILE.default.permissions,
         msg.member?.permissions.toArray()!
@@ -29,10 +29,10 @@ const event: Event = {
       if (!COMMAND_FILE.default.permissions || userHasPermissions) {
         COMMAND_FILE.default.run(client, msg, ARGUMENTS);
       } else {
-        msg.channel.send("You do not have permissions to use this command.");
+        msg.channel.send('You do not have permissions to use this command.');
       }
-    } catch (error) {
-      console.error;
+    } catch (e) {
+      return;
     }
   },
 };
