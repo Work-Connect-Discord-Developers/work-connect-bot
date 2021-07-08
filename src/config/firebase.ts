f6bb56a0ea0db19339909c7213ef7e15e7fa6be3
Adonys Santos <adonysmundotech@gmail.com>
2021-07-07 21:53:05 -0400

    refactor: Format documents

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,14 +1,14 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
-import getEnvVar from "./helpers/getEnvVar";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import getEnvVar from './helpers/getEnvVar';
 
-const apiKey = getEnvVar("FIREBASE_API_KEY");
-const authDomain = getEnvVar("FIREBASE_AUTH_DOMAIN");
-const projectId = getEnvVar("FIREBASE_PROJECT_ID");
-const storageBucket = getEnvVar("FIREBASE_STORAGE_BUCKET");
-const messagingSenderId = getEnvVar("FIREBASE_MESSAGING_SENDER_ID");
-const appId = getEnvVar("FIREBASE_APP_ID");
-const measurementId = getEnvVar("FIREBASE_MEASUREMENT_ID");
+const apiKey = getEnvVar('FIREBASE_API_KEY');
+const authDomain = getEnvVar('FIREBASE_AUTH_DOMAIN');
+const projectId = getEnvVar('FIREBASE_PROJECT_ID');
+const storageBucket = getEnvVar('FIREBASE_STORAGE_BUCKET');
+const messagingSenderId = getEnvVar('FIREBASE_MESSAGING_SENDER_ID');
+const appId = getEnvVar('FIREBASE_APP_ID');
+const measurementId = getEnvVar('FIREBASE_MEASUREMENT_ID');
 
 const firebaseConfig = {
   apiKey,
@@ -23,4 +23,4 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
-export default db
+export default db;
