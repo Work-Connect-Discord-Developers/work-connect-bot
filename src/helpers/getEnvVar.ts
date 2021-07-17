eb42c418e60412f970a401db7ede4b646a106f2f
Adonys Santos <adonyssantosjimenez@gmail.com>
2021-07-17 00:30:02 -0400

    fix: [object Object].BOT_PREFIX is undefined!

--- src/helpers/getEnvVar.ts
+++ src/helpers/getEnvVar.ts
@@ -1,15 +1,17 @@
-// Environment Variables
 import { config } from 'dotenv';
-if (process.env.NODE_ENV !== 'production') {
-  const NODE_ENV = process.env.NODE_ENV;
-  config({ path: `.env.${NODE_ENV}` });
-}
+
+// Environment Variables
+const NODE_ENV = process.env.NODE_ENV;
+config({ path: `.env.${NODE_ENV}` });
 
 const getEnvVar = (value: string): string => {
   const ret = process.env[value];
-  if (ret === undefined)
+
+  if (ret === undefined) {
     throw new Error(`${process.env}.${value} is undefined!`);
-  return ret;
+  } else {
+    return ret;
+  }
 };
 
 export default getEnvVar;
